f1f9fcc872c09d09280e4a2777862f584cfb944e
Rerison-Alves <107881260+Rerison-Alves@users.noreply.github.com>
2024-07-08 17:48:10 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Iniciar o servidor Minecraft com 4GB de memória inicial e máxima
-java -Xms4096M -Xmx4096M -jar server.jar nogui
+java -Xms2048M -Xmx2048M -jar server.jar nogui
 
 # Pausa para manter o script ativo (opcional)
 read -p "Pressione Enter para continuar..."
