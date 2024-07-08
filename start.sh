1bd7a84936d13e0ee4ec37bd0a8cc95dfece91ae
Rerison-Alves <rerison.alves07@aluno.ifce.edu.br>
2024-07-08 17:46:33 -0300

    save 08/07/24

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Iniciar playit em segundo plano
-nohup playit > playit.log 2>&1 &
-
 # Iniciar o servidor Minecraft com 4GB de memória inicial e máxima
 java -Xms4096M -Xmx4096M -jar server.jar nogui
 
