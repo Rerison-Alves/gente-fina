8d678125c430e50698bdfcdaf28bacaa875d671b
Rerison-Alves <rerison.alves07@aluno.ifce.edu.br>
2024-07-08 12:29:22 -0300

    correção bash e ip

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Iniciar playit em segundo plano
-playit &
+nohup playit > playit.log 2>&1 &
 
 # Iniciar o servidor Minecraft com 4GB de memória inicial e máxima
 java -Xms4096M -Xmx4096M -jar server.jar nogui
